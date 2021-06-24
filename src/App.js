ca4957a9234ce9482b0910e36deabd32b8520ba8
Felipe Quintero <ffquint@gmail.com>
2021-06-24 19:23:44 -0400

    Routes in App were simplified per MVP requirements. MVP has been reached.

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
 				<CartContext.Provider value={{cart}}>
-					<Navigation cart={cart} />
+					<Navigation />
 
 					{/* Routes */}
 					<Route exact path="/">
@@ -30,7 +30,7 @@ function App() {
 					</Route>
 
 					<Route path="/cart">
-						<ShoppingCart cart={cart} />
+						<ShoppingCart />
 					</Route>
 				</CartContext.Provider>
 			</ProductContext.Provider>
